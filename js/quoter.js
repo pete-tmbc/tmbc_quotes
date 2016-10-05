13a3d78505c1bc6dced633b27f2f721c8fa39697
petri <petri.iivonen@tmbc.gov.uk>
2016-10-05 09:05:14 +0100

    added submit function

--- js/quoter.js
+++ js/quoter.js
@@ -1,8 +1,10 @@
-var randQuote;
-var quotJson = 'json/quotes.json';
-$.getJSON(quotJson,function(quotes){
-    randQuote = quotes.entries[Math.floor(Math.random() * quotes.entries.length)];
-    $('#loaderoverlay').empty().append("<p>&#x201C;" + randQuote.quote + "&#x201D;</p>");
-    // $('#qauth').empty().append(randQuote.author);
-    $('#loaderoverlay').append("<cite>&#x2013; &#x2013; " + randQuote.author + "</cite>");
+$("form").submit(function() {
+    var randQuote;
+    var quotJson = 'json/quotes.json';
+    $.getJSON(quotJson,function(quotes){
+        randQuote = quotes.entries[Math.floor(Math.random() * quotes.entries.length)];
+        $('#loaderoverlay').empty().append("<p>&#x201C;" + randQuote.quote + "&#x201D;</p>");
+        // $('#qauth').empty().append(randQuote.author);
+        $('#loaderoverlay').append("<cite>&#x2013; &#x2013; " + randQuote.author + "</cite>");
+    });
 });
